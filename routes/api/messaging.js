e5fc7e255dbfa72733a75f9f22288d146ec67254
davitabosa12 <davitabosa12@hotmail.com>
2018-11-10 14:52:31 -0300

    envio de notificacoes para aluno

--- routes/api/messaging.js
+++ routes/api/messaging.js
@@ -10,12 +10,13 @@ var express = require('express');
 var auth = require('./auth');
 var firebase = require('../../secret/firebase');
 var MessageDAO = require('../../models/message/message');
+var AlunoDAO = require('../../models/aluno/alunoDAO');
 var router = express.Router();
 
 
 //router.use(auth);
 
-router.post('/:topic', auth, (req, res, next) => {
+router.post('/:topic', (req, res, next) => {
     var topic = req.params.topic;
     var currentUser = req.userData.currentUser;
     console.log(`User ${currentUser.name} tried to send message to topic ${topic}`);
@@ -25,6 +26,40 @@ router.post('/:topic', auth, (req, res, next) => {
     });
 
     //TODO: Apenas professores e alunos especiais podem realizar mensagens a topicos
+});
+
+//envia uma mensagem para um aluno, com seu fcm token...
+router.post("/aluno/:matricula", (req, res, next) => {
+    const titulo = req.body.titulo;
+    const mensagem = req.body.mensagem;
+    AlunoDAO.findOne({
+        matricula: req.params.matricula
+    }).exec()
+    .then(aluno => {
+        const theToken = aluno.tokenFCM;
+
+        firebase.messaging().send({
+            notification: {
+                body: mensagem,
+                title: titulo,
+            },
+            token: theToken
+        }).then(() => {
+            res.send(200).json({
+                mensagem: `Mensagem para aluno ${req.params.matricula} enviado com sucesso`
+            });
+        }).catch(() => {
+            res.send(500).json({
+                mensagem: `Erro ao enviar mensagem: firebase`
+            })
+        })
+    })    
+    .catch(err => {
+        res.send(500).json({
+            mensagem: `Erro ao enviar mensagem: mongo`,
+            motivo: err
+        });
+    });
 });
 
 router.post('/:topic', (req, res, next) => {
